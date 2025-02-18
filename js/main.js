d7972d99ed99215af4f066c488533b3de41da44e
Achmad F. Ibrahim <acfatah@gmail.com>
2025-02-18 11:27:12 +0800

    Add example script

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
 console.log('Script loaded')
+
+const params = new URLSearchParams(window.location.search);
+const submittedValues = {
+  text: params.get('text'),
+  password: params.get('password'),
+  number: params.get('number'),
+  email: params.get('email'),
+  date: params.get('date'),
+  color: params.get('color'),
+  select: params.get('select'),
+  textarea: params.get('textarea'),
+}
+
+console.group( 'Submitted values:',)
+console.log(submittedValues)
+console.groupEnd()
